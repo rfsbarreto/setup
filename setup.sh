255b04614e75396c857cfab3824cc81ee82f10bf
Raphael Barreto <rfsbarreto@gmail>
2014-03-10 21:20:44 -0300

    Add: Suíte LibreOffice

--- setup.sh
+++ setup.sh
@@ -9,16 +9,24 @@ wget -O - http://deb.opera.com/archive.key | sudo apt-key add -
 
 
 # Java jdk
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz"
---2013-10-30 01:59:28--  download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz 
+#wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz"
+#--2013-10-30 01:59:28--  download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz 
 
-mkdir /usr/java/
-tar -xvzf jdk-7u45-linux-64.tar.gz -C /usr/java/
-update-alternatives --install "/usr/bin/java" "java" "/usr/java/jdk1.7.0_45/jre/bin/java" 1
-update-alternatives --config java
+#mkdir /usr/java/
+#tar -xvzf jdk-7u45-linux-64.tar.gz -C /usr/java/
+#update-alternatives --install "/usr/bin/java" "java" "/usr/java/jdk1.7.0_45/jre/bin/java" 1
+#update-alternatives --config java
 
 
 
-#apt-gets
+#####apt-gets########
 apt-get update
-apt-get install pidgin comix qbittorrent clementine opera ubuntu-restricted-extras faac faad ffmpeg ffmpeg2theora flac icedax id3v2 lame libflac++6 libjpeg-progs libmpeg3-1 mencoder mjpegtools mp3gain mpeg2dec mpeg3-utils mpegdemux mpg123 mpg321 regionset sox uudeview vorbis-tools x264 arj p7zip p7zip-full p7zip-rar rar unrar unace-nonfree vlc nautilus-dropbox k3b gedit-plugins vim guake
+
+# Bibliotecas  para o adb Android
+apt-get install libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5
+
+# Office
+apt-get libreoffice
+
+# Gerais
+apt-get install comix qbittorrent clementine opera ubuntu-restricted-extras faac faad ffmpeg ffmpeg2theora flac icedax id3v2 lame libflac++6 libjpeg-progs libmpeg3-1 mencoder mjpegtools mp3gain mpeg2dec mpeg3-utils mpegdemux mpg123 mpg321 regionset sox uudeview vorbis-tools x264 arj p7zip p7zip-full p7zip-rar rar unrar unace-nonfree vlc nautilus-dropbox k3b gedit-plugins vim guake
